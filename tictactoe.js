598af8c38097cd9d687b88a4fbb48f2a6e017178
Sep Bar <sepand.barkhodaee@gmail.com>
2014-02-19 22:56:18 -0500

    completed TicTacToe

--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	var i = 1;
 
 	var isOdd = function(number){
 		if (number%2 == 0){
@@ -8,24 +9,50 @@ $(document).ready(function(){
 		}
 	}
 
-	var checkWinner = function(){
-
-
-	}
-	var i = 1;
-	var selection = function() {
-		if ($(this).html() === "") {
-			if (isOdd(i)) {
-				$(this).html("O");
-				i++;
-			}else{
-				$(this).html("x");
-				i++;
-			}
+	var checkThree = function(one, two, three){
+		if (one === two && two === three && one !== "") {
+			alert("winner!");
+			i = 1;
+			$('td').empty();
 		}
 	}
 
-	$('td').click(selection);
+
+	var checkWinner = function(){
+		checkThree($('#11').html(), $('#12').html(), $('#13').html());
+		checkThree($('#21').html(), $('#22').html(), $('#23').html());
+		checkThree($('#31').html(), $('#32').html(), $('#33').html());
+		checkThree($('#11').html(), $('#21').html(), $('#31').html());
+		checkThree($('#12').html(), $('#22').html(), $('#32').html());
+		checkThree($('#31').html(), $('#32').html(), $('#33').html());
+		checkThree($('#11').html(), $('#22').html(), $('#33').html());
+		checkThree($('#31').html(), $('#22').html(), $('#13').html());
+	}
+
+	var selection = function() {
+		if ($(this).html() === "") {
+			if (isOdd(i)) {
+				$(this).html("X");
+				i++;
+				checkWinner();
+			}else{
+				$(this).html("O");
+				i++;
+				checkWinner();
+			}
+
+		}
+		console.log(i);
+		if (i === 10){
+			i = 1;
+			$('td').empty();
+			alert("NEW GAME FROM DOWN BELOW")
+		}
+
+	}
+	$('td').click(selection)
+	//$('td').bind('click ', selection);
+
 
 
 })
